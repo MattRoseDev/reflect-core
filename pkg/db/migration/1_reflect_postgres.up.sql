55139011534f064934b108210a9d37a2c2cd2dd4
Matt Rose <mattroseinbox@gmail.com>
2021-04-10 03:38:59 +0430

    perf: auto timestamp for updated_at fields

--- pkg/db/migration/1_reflect_postgres.up.sql
+++ pkg/db/migration/1_reflect_postgres.up.sql
@@ -7,8 +7,8 @@ CREATE TABLE "user" (
 	"fullname" TEXT,
 	"bio" TEXT,
 	"admin" BOOLEAN NOT NULL DEFAULT FALSE,
-	"created_at" TIMESTAMP NOT NULL DEFAULT (now()),
-	"updated_at" TIMESTAMP NOT NULL DEFAULT (now()),
+	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
 	"deleted_at" TIMESTAMP,
 	CONSTRAINT "user_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -21,8 +21,8 @@ CREATE TABLE "password" (
 	"id" uuid NOT NULL DEFAULT uuid_generate_v4(),
 	"user_id" uuid NOT NULL,
 	"password" TEXT NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT (now()),
-	"updated_at" TIMESTAMP NOT NULL DEFAULT (now()),
+	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
 	"deleted_at" TIMESTAMP,
 	CONSTRAINT "password_pk" PRIMARY KEY ("id")
 ) WITH (
@@ -36,18 +36,26 @@ CREATE TABLE "post" (
 	"user_id" uuid NOT NULL,
 	"content" TEXT NOT NULL,
 	"link" varchar(12) NOT NULL,
-	"created_at" TIMESTAMP NOT NULL DEFAULT (now()),
-	"updated_at" TIMESTAMP NOT NULL DEFAULT (now()),
+	"created_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
+	"updated_at" TIMESTAMP NOT NULL DEFAULT (NOW()),
 	"deleted_at" TIMESTAMP,
 	CONSTRAINT "post_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE OR REPLACE FUNCTION trigger_set_updated_at()   
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at= now();
+    RETURN NEW;   
+END;
+$$ language 'plpgsql';
 
-
+CREATE TRIGGER update_user BEFORE UPDATE ON "user" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
+CREATE TRIGGER update_password BEFORE UPDATE ON "password" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
+CREATE TRIGGER update_post BEFORE UPDATE ON "post" FOR EACH ROW EXECUTE PROCEDURE  trigger_set_updated_at();
 
 ALTER TABLE "password" ADD CONSTRAINT "password_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
 
 ALTER TABLE "post" ADD CONSTRAINT "post_fk0" FOREIGN KEY ("user_id") REFERENCES "user"("id");
-
